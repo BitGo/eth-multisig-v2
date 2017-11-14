2a14ae1e2e4cf9d8998d873a1d889e3f59086349
Otto Allmendinger <otto@bitgo.com>
2017-11-14 11:56:32 +0100

    Raise gas limit to make truffle work

--- truffle.js
+++ truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      // https://github.com/trufflesuite/truffle/issues/271#issuecomment-341651827
+      gas: 2900000
     }
   }
 };
