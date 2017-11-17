01e86e3c9fb510883b628fc7608f92af0257995b
Otto Allmendinger <otto@bitgo.com>
2017-11-17 13:14:43 +0100

    Split tests into truffle-test, mocha-test
    Summary:
    Certain things are hard to test with truffle, so we need a separate
    test set.
    
    Reviewers: barath
    
    Reviewed By: barath
    
    Differential Revision: https://phabricator.bitgo.com/D7174

---
+++ mocha-test/compile.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const _ = require('lodash');
+const should = require('should');
+
+const solc = require('solc');
+
+// Useful when async func goes wrong
+process.on('unhandledRejection', (r) => {
+  console.log('UnhandledRejection');
+  console.error(r);
+});
+
+describe('Contracts', async () => {
+  const contracts = [
+    'ERC20Interface.sol',
+    'FixedSupplyToken.sol',
+    'Forwarder.sol',
+    'WalletSimple.sol'
+  ];
+
+  let result;
+
+  before(async function () {
+    // solc takes a while
+    this.timeout(10000);
+    const contents = await Promise.all(contracts.map(async (filename) =>
+      (await promisify(fs.readFile)('./contracts/' + filename)).toString()
+    ));
+    const sources = _.zipObject(contracts, contents);
+    result = solc.compile({ sources }, 1);
+  });
+
+  it('compile without warnings and errors', () => {
+    should.equal(
+      (result.errors || []).length, 0,
+      (result.errors || []).join('\n')
+    );
+  });
+});
