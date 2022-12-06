df3c7bbcc06efc62a70e284affe13cfb7e4ab28c
Mullapudi Pruthvik <mullapudipruthvik@bitgo.com>
2022-12-06 09:00:44 +0530

    feat(contracts): make v0 contracts similar to v1
    Ticket: BG-62483

--- recovery-contracts/RecoveryForwarder.sol
+++ recovery-contracts/RecoveryForwarder.sol
@@ -1,23 +1,24 @@
 pragma solidity 0.8.10;
-
+import './TransferHelper.sol';
+import './ERC20Interface.sol';
 /**
- * Basic singleSig contract designed to send funds controlled by signer.
+ * Contract that will forward any incoming Ether to the creator of the contract
  */
 contract RecoveryForwarder {
-  // Address which can move funds from this contract
-  address public signer;
+  // Address to which any funds sent to this contract will be forwarded
+  address public parentAddress;
   /**
-   * Initialize the signer
+   * Initialize the parent
    */
-  function init(address _signer) external onlyUninitialized {
-    signer = _signer;
+  function init(address _parentAddress) external onlyUninitialized {
+    parentAddress = _parentAddress;
   }
 
-   /**
-   * Modifier that will execute internal code block only if the sender is an authorized signer on this wallet
-   */
-  modifier onlySigner {
-    require( signer == msg.sender, 'Non-signer in onlySigner method');
+  /**
+  * Modifier that will execute internal code block only if the sender is the parent address
+  */
+  modifier onlyParent {
+    require( parentAddress == msg.sender, 'Non-parent in onlyParent method');
     _;
   }
 
@@ -25,36 +26,59 @@ contract RecoveryForwarder {
    * Modifier that will execute internal code block only if the contract has not been initialized yet
    */
   modifier onlyUninitialized {
-    require(signer == address(0x0), 'Already initialized');
+    require(parentAddress == address(0x0), 'Already initialized');
     _;
   }
 
-   /**
-   * Default function; Gets called when Ether is deposited
-   */
-   receive() external payable {
+  /**
+  * Default function; Gets called when Ether is deposited
+  */
+  receive() external payable {
+    flush();
   }
 
   /**
    * Default function; Gets called when data is sent but does not match any other function
    */
   fallback() external payable {
+    flush();
   }
 
   /**
-   * Execute a transaction from this contract using the signer.
-   *
-   * @param toAddress the destination address to send an outgoing transaction
-   * @param value the amount in Wei to be sent
-   * @param data the data to send to the toAddress when invoking the transaction
+   * Execute a token transfer of the full balance from the forwarder token to the parent address
+   * @param tokenContractAddress the address of the erc20 token contract
    */
-  function sendFunds(
-      address toAddress,
-      uint256 value,
-      bytes calldata data
-  ) external onlySigner {
-    // Success, send the transaction
-   (bool success, ) = toAddress.call{ value: value }(data);
-    require(success, 'Call execution failed');
+  function flushTokens(address tokenContractAddress) external onlyParent {
+    ERC20Interface instance = ERC20Interface(tokenContractAddress);
+    address forwarderAddress = address(this);
+    uint256 forwarderBalance = instance.balanceOf(forwarderAddress);
+    TransferHelper.safeTransfer(
+      tokenContractAddress,
+      parentAddress,
+      forwarderBalance
+    );
+  }
+
+  /**
+   * A fallback function which can used to transfer funds controlled by parent.
+   */
+  function callFromParent(
+  address target,
+  uint256 value,
+  bytes calldata data
+  ) external onlyParent {
+    (bool success, ) = target.call{ value: value }(
+      data
+    );
+    require(success, 'Parent call execution failed');
+  }
+
+  /**
+   * Flush the entire balance of the contract to the parent address.
+   */
+  function flush() public {
+    uint256 value = address(this).balance;
+    (bool success, ) = parentAddress.call{ value: value }('');
+    require(success, 'Flush failed');
   }
 }
